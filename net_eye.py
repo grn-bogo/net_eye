fab51c39f8d47a074a71e0f50d53e8e04f69597d
Arturo Ruiz Mañas <arruma2160@gmail.com>
2018-06-15 23:35:09 +0200

    added logging system and logging out at net_eye.py main file

--- net_eye.py
+++ net_eye.py
@@ -1,7 +1,26 @@
 #! /usr/bin/env python3
+import logging
+
+
+def create_logger(name):
+    '''
+
+    '''
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
 
 
 def main(argv):
+    '''
+
+    '''
     from argparse import ArgumentParser
     from pcap_parser.tcp_parsing import TCPTrafficSequence
     parser = ArgumentParser()
@@ -26,16 +45,33 @@ def main(argv):
             dest="tcp_port")
     args = parser.parse_args()
 
-    sequence = TCPTrafficSequence(srv_port=args.tcp_port,
-                                  srv_ip=args.srv_ip,
-                                  file_name=args.pcap_path)
+    logger.info("  @@@@@@@@@@@@@@@@@@@@@@@@@")
+    logger.info("  -------------------------")
+    logger.info("||    N E T   --  E Y E    ||")
+    logger.info("  -------------------------")
+    logger.info("  @@@@@@@@@@@@@@@@@@@@@@@@@")
+
+    logger.debug("Arguments: -----")
+    for arg in vars(args):
+        logger.debug("arg[{k}] = {v}".format(k=arg, v=getattr(args, arg)))
+    logger.debug("----------------")
+
+    sequence = TCPTrafficSequence(
+                    srv_port=args.tcp_port,
+                    srv_ip=args.srv_ip,
+                    file_name=args.pcap_path)
     sequence.init()
-    print("PAYLOAD: {0}, OVERHEAD {1}".format(
-        sequence.payload_size, sequence.overhead_size))
-    print("PAYLOAD TO OVERHEAD {0}".format(sequence.payload_ratio))
+
+    logger.info(
+        "PAYLOAD: {0}, OVERHEAD {1}".format(
+            sequence.payload_size, sequence.overhead_size))
+    logger.info(
+        "PAYLOAD TO OVERHEAD {0}".format(sequence.payload_ratio))
+
     sequence.create_statistics_files()
 
 
 if __name__ == '__main__':
     import sys
+    logger = create_logger('net_eye')
     main(sys.argv)
