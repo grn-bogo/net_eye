22793294d1049c23512fa8895bce7a4e75ba6074
Arturo Ruiz Mañas <arruma2160@gmail.com>
2018-06-15 23:04:15 +0200

    generate_proto importing from general@utils/

--- generate_proto.py
+++ generate_proto.py
@@ -1,8 +1,7 @@
-import general_utils
+from utils.general import git_root, \
+                          generate_proto
 import os
 if __name__ == '__main__':
 
-    proto_root = os.path.join(general_utils.git_root()[0], 'protobuf_defs')
-    general_utils.generate_proto(proto_root)
-
-
+    proto_root = os.path.join(git_root()[0], 'protobuf_defs')
+    generate_proto(proto_root)
